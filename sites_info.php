4b38a2762982995eea157582c47fbdb7e03b6ee6
Juanfran <juanfran.granados@mirai.com>
2020-09-02 11:52:24 +0200

    Make the command multidomain.

--- sites_info.php
+++ sites_info.php
@@ -22,24 +22,25 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
 			$dmtable = $this->db->base_prefix . 'domain_mapping';
 
-			$dm_domain = $args[0];
+			foreach ($args as $dm_domain ) {
 
-			if( ( $nowww = preg_replace( '|^www\.|', '', $dm_domain ) ) != $dm_domain )
-				$where = $this->db->prepare( 'domain IN (%s,%s)', $dm_domain, $nowww );
-			else
-				$where = $this->db->prepare( 'domain = %s', $dm_domain );
+				// Add www. if not present.
+				if( substr( $dm_domain, 0, 4 ) !== 'www.' ) {
+					$dm_domain = 'www.' . $dm_domain;
+				}
 
-			$site_id = $this->db->get_var( "SELECT blog_id FROM {$dmtable} WHERE {$where} ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1" );
+				$where   = $this->db->prepare( 'domain = %s', $dm_domain );
+				$site_id = $this->db->get_var( "SELECT blog_id FROM {$dmtable} WHERE {$where} AND active = 1 ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1" );
 
-			$options = array(
-			  'return'     => true
-			);
-			$theme = WP_CLI::runcommand( 'option get current_theme --url=' . $args[0], $options );
+				$options = array(
+				  'return' => true,
+				);
+				$theme = WP_CLI::runcommand( 'option get current_theme --url=' . $dm_domain, $options );
+				$url = WP_CLI::runcommand( 'site list --site__in=' . $site_id . ' --field=url', $options );
 
-			$url = WP_CLI::runcommand( 'site list --site__in=' . $site_id . ' --field=url', $options );
-
-			$parsed_url = wp_parse_url( $url );
-			echo "$site_id, {$args[0]}, {$parsed_url['host']}, $theme\n";
+				$parsed_url = wp_parse_url( $url );
+				echo "$site_id, {$dm_domain}, {$parsed_url['host']}, $theme\n";
+			}
 		}
 	}
 
